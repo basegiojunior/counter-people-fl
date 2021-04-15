ca4a05170fe5527498bdddaadc4f0b04b501b459
Ademir Baségio Junior <basegiojunior@gmail.com>
2021-04-14 22:46:24 -0300

    feat: set int and String states

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,19 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   int _people = 1;
+  String _infoText = "Pode entrar!";
 
   void _changePeple(int delta) {
     setState(() {
       _people += delta;
+
+      if (_people < 0) {
+        _infoText = "Mundo invertido?";
+      } else if (_people <= 10) {
+        _infoText = "Pode entrar!";
+      } else {
+        _infoText = "Lotado!";
+      }
     });
   }
 
@@ -62,7 +71,7 @@ class _HomeState extends State<Home> {
                 )
               ],
             ),
-            Text("Pode entrar!",
+            Text(_infoText,
                 style: TextStyle(
                     color: Colors.white,
                     fontWeight: FontWeight.bold,
